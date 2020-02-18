a28dcc25934d7db1ce4f7c010a180f213596ec02
Paul Leclerc <paulleclerc@icloud.com>
2020-02-18 17:26:56 +0100

    Initedproject

---
+++ src/androidTest/java/com/openclassrooms/magicgithub/ui/user_list/ListUserActivityTest3.java
@@ -0,0 +1,22 @@
+package com.openclassrooms.magicgithub.ui.user_list;
+
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ListUserActivityTest3 {
+
+    @Rule
+    public ActivityTestRule<ListUserActivity> mActivityTestRule = new ActivityTestRule<>(ListUserActivity.class);
+
+    @Test
+    public void listUserActivityTest3() {
+    }
+}
